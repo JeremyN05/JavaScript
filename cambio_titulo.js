ab88074350dc736eeec14d53aeb8b8034f5529a7
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-16 19:41:28 +0200

    Error

--- cambio_titulo.js
+++
@@ -1,9 +0,0 @@
-function cambiarTitulo() {
-    var nuevoTitulo = prompt("Introduce el nuevo título:");
-    if (nuevoTitulo !== null && nuevoTitulo !== "") {
-        document.title = nuevoTitulo;
-    }
-}
-
-document.getElementById("cambiarTituloBtn").addEventListener("click", cambiarTitulo);
-
