fa9ca76dfc8fa62d1e4f970368dac7955b296de9
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-16 19:45:24 +0200

    Cambiar título

---
+++ Cambiar título/cambio_titulo.js
@@ -0,0 +1,8 @@
+function cambiarTitulo() {
+    var nuevoTitulo = prompt("Introduce el nuevo título:");
+    if (nuevoTitulo !== null && nuevoTitulo !== "") {
+        document.title = nuevoTitulo;
+    }
+}
+
+document.getElementById("cambiarTituloBtn").addEventListener("click", cambiarTitulo);
